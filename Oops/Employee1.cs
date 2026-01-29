9081a639092b727b7e074ef0f46e9be7e9ac7335
pavan-patil8861 <pavanpatil8861@gmail.com>
2026-01-29 17:37:45 +0530

    Add files via upload

---
+++ Oops/Employee1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Oops
+{
+    internal class Employee1
+    {
+        public int Id = 101;
+        public string Name = "Rani";
+        public decimal Salary = 90000;
+
+        public decimal IncrementSalary(double per)
+        {
+            Salary = Salary + (Salary * (decimal)(per / 100));
+            return Salary;
+        }
+
+        public void DisplayEmployee1Details()
+        {
+            Console.WriteLine("Employee Id: " + Id);
+            Console.WriteLine("Employee Name: " + Name);
+            Console.WriteLine("Employee Salary: " + Salary);
+            Console.WriteLine("-----------------------------");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee1 emp1 = new Employee1();
+
+            emp1.DisplayEmployee1Details();
+            emp1.IncrementSalary(10);
+            emp1.DisplayEmployee1Details();
+        }
+    }
+}
+
